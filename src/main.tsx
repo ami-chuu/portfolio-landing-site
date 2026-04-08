93198f8e6a80d313e4aa797c7fffdaa4b6756eca
ami-chuu <amichuu73@proton.me>
2026-04-08 18:58:22 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,18 @@
-import { createRoot } from 'react-dom/client'
-import { HashRouter, Routes, Route } from 'react-router-dom'
-import './index.css'
-import Home from './Home'
-import Polar from './Polar'
+import { createRoot } from 'react-dom/client';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import './index.css';
+import Home from './Home';
+import Polar from './Polar';
+import App from './App';
 
-const App = () => (
+const Root = () => (
   <HashRouter>
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<App />} />
       <Route path="/polar" element={<Polar />} />
+      <Route path="/home" element={<Home />} />
     </Routes>
   </HashRouter>
-)
+);
 
-createRoot(document.getElementById('root')!).render(
-  <App />
-)
+createRoot(document.getElementById('root')!).render(<Root />);
