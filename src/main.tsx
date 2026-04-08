bc7b13a6a7d0c37764a5f2b9fb94578723cb57de
ami-chuu <amichuu73@proton.me>
2026-04-08 18:49:30 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,38 +1,18 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
-import Home from './Home.tsx'
-import { RouterProvider, createBrowserRouter} from 'react-router-dom';
-import Polar from './Polar.tsx';
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import { HashRouter } from 'react-router-dom'
+import Home from './Home'
+import Polar from './Polar'
 
-const router = createBrowserRouter(
-  [
-    {
-      path: '/',
-      element: <Home />,
-      handle: {
-        title: "ИНГРИЯТЕХ - главная страницы"
-      }
-    },
-    {
-      path: '/polar',
-      element: <Polar />,
-      handle: {
-        title: "Поляр"
-      }
-    }
-  ]
+const App = () => (
+  <HashRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/polar" element={<Polar />} />
+    </Routes>
+  </HashRouter>
 )
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
-  <HashRouter>
-    <App />
-  </HashRouter>
+  <App />
 )
