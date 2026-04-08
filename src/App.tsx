a299b90a21ce469ab57791b821cf3d354d4bf350
ami-chuu <amichuu73@proton.me>
2026-04-08 18:44:19 +0300

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Home from './Home';
+import Polar from './Polar';
+import './App.css';
+
+export default function App() {
+  return (
+    <div>
+      <Home />
+      <Polar />
+    </div>
+  );
+}
