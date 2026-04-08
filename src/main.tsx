32e1677c6695c8a14f7fb3d511a14f9fba292a73
ami-chuu <amichuu73@proton.me>
2026-04-08 18:32:45 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,10 @@ import './index.css'
 import Home from './Home.tsx'
 import { RouterProvider, createBrowserRouter} from 'react-router-dom';
 import Polar from './Polar.tsx';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import { BrowserRouter } from 'react-router-dom'
 
 const router = createBrowserRouter(
   [
@@ -28,4 +32,10 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <RouterProvider router={router} />
   </StrictMode>,
+)
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <App />
+  </BrowserRouter>
 )
